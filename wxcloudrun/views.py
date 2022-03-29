1fa66b0f2da02c52cf1fca2f3d1a2651476b69a6
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-03-29 20:07:12 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -25,5 +25,6 @@ def upload():
     mask:设置背景图片   background_color:设置背景颜色
     scale:按照比例进行放大画布，此处指长和宽都是原来画布的1.5倍
     generate(text)：根据文本生成词云 '''
-    wordcloud.to_file('/app/wxcloudrun/ciyun.jpg')
     return "ok"
+    wordcloud.to_file('/app/wxcloudrun/ciyun.jpg')
+    
