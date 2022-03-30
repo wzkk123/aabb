e2657c9325a1708bf25c24ec1a9528534fba0e6e
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-03-30 18:37:06 +0800

    Add files via upload

---
+++ wxcloudrun/im.py
@@ -0,0 +1,30 @@
+import json
+
+import requests
+def duixiangcunchu():
+  #获取token
+  response = requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxfb2997f507abf89e&secret=168726b557fb7221c96955cec59b3347',)
+  print()
+  data ={
+    "env": "prod-0gayxkvve034fe60",
+    "path": "ciyunimage/ciyun.jpg"
+  }
+  #转json
+  data = json.dumps(data)
+  response = requests.post("https://api.weixin.qq.com/tcb/uploadfile?access_token="+response.json()['access_token'],data)
+  print(response.json())
+  #response = json.loads(response.json())
+  #得到上传链接
+
+  data2={
+    "Content-Type":(None,".jpg"),
+    "key": (None,"ciyunimage/ciyun.jpg"),
+    "Signature": (None,response.json()['authorization']),
+    'x-cos-security-token': (None,response.json()['token']),
+    'x-cos-meta-fileid': (None,response.json()['cos_file_id']),
+    'file': ('ciyun.jpg',open(r"D:\Study\pycharmpro\project\image\1.png","rb"))
+  }
+  #data2 = json.dumps(data2)
+  response2 = requests.post(response.json()['url'], files=data2)
+  print(response.json()["file_id"])
+
